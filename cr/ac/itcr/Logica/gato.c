8cd3d45da35fcb2dfee07c8aad703113d1081d21
jonathan <jonagaru1@hotmail.com>
2016-05-31 13:54:36 -0600

    pruebas

---
+++ cr/ac/itcr/Logica/gato.c
@@ -0,0 +1,5 @@
+//
+// Created by jonathan on 5/31/16.
+//
+
+#include "gato.h"
---
+++ cr/ac/itcr/Logica/gato.h
@@ -0,0 +1,8 @@
+//
+// Created by jonathan on 5/31/16.
+//
+
+#ifndef EPIC_DRIVE_GATO_H
+#define EPIC_DRIVE_GATO_H
+
+#endif //EPIC_DRIVE_GATO_H
